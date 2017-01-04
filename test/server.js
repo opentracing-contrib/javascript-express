c240e59c02ec8f40c55125954174ed9fd5e6b3b9
Rafael Garcia <rafael.garcia@clever.com>
2017-01-04 01:20:52 +0000

    revert opentracing upgrade

--- test/server.js
+++ test/server.js
@@ -1,19 +1,63 @@
 const assert = require("assert");
 const opentracing = require("opentracing");
-const {MockTracer} = require("opentracing/lib/mock_tracer");
 const express = require("express");
 const middleware = require("../dist/index.js").default;
 const request = require("request");
+const LightStep = require("lightstep-tracer");
+const sinon = require("sinon");
 
 describe("e2e express app", () => {
-  it("works", (done) => {
-    // create and start an express server, hit it with a request
-    const mockTracer = new MockTracer();
-    const noopTracer = new opentracing.Tracer();
-    mockTracer._extract = noopTracer._extract;
-    mockTracer._inject = noopTracer._inject;
+  it("works with default tracer", (done) => {
     const app = express();
-    app.use(middleware({tracer: mockTracer}));
+    app.use(middleware({}));
+    const startSpanSpy = sinon.spy(opentracing, "startSpan");
+
+    let reqSpanPresent = false;
+    app.get("/", (req, res) => {
+      if (req.span) {
+        reqSpanPresent = true;
+      }
+      res.send("Hello World!")
+    });
+
+    const server = app.listen(3000, (err) => {
+      const opts = {
+        url : "http://localhost:3000/",
+        method: "GET",
+      };
+      request(opts, (err, res, body) => {
+        assert.ifError(err);
+        assert.equal(res.statusCode, 200, body);
+        assert(reqSpanPresent, "expected req.span to be set");
+        assert(startSpanSpy.calledOnce, "expected only one span");
+        // TODO: once lightstep supports opentracing 0.13.0, we can use
+        // mocktracer and verify some more details on the span itself
+        //const span = mockTracer._spans[0];
+        //assert.deepEqual(span._tags, {
+        //  "http.method": "GET",
+        //  "span.kind": "server",
+        //  "http.url": "/",
+        //  "http.status_code": 200,
+        //});
+        //assert.equal(span._operationName, "/");
+        //assert.equal(span._logs.length, 2, "expected two logs in the span");
+        //const [startLog, finishLog] = span._logs;
+        //assert.equal(startLog.fields.event, "request_received");
+        //assert.equal(finishLog.fields.event, "request_finished");
+        server.close();
+        done();
+      });
+    });
+  });
+
+
+  it("works with latest lightstep tracer", (done) => {
+    const lsTracer = LightStep.tracer({
+      access_token   : 'foo',
+      component_name : 'bar',
+    });
+    const app = express();
+    app.use(middleware({tracer: lsTracer}));
 
     var reqSpanPresent = false;
     app.get("/", (req, res) => {
@@ -32,19 +76,6 @@ describe("e2e express app", () => {
         assert.ifError(err);
         assert.equal(res.statusCode, 200, body);
         assert(reqSpanPresent, "expected req.span to be set");
-        assert.equal(mockTracer._spans.length, 1, "expected only one span");
-        const span = mockTracer._spans[0];
-        assert.deepEqual(span._tags, {
-          "http.method": "GET",
-          "span.kind": "server",
-          "http.url": "/",
-          "http.status_code": 200,
-        });
-        assert.equal(span._operationName, "/");
-        assert.equal(span._logs.length, 2, "expected two logs in the span");
-        const [startLog, finishLog] = span._logs;
-        assert.equal(startLog.fields.event, "request_received");
-        assert.equal(finishLog.fields.event, "request_finished");
         server.close();
         done();
       });
